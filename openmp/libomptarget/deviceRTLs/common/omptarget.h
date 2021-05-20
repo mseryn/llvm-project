5b1cc988b6a9f000f10eac61587da047e2e6f466
Melanie Cornelius <melanie.e.cornelius@gmail.com>
2021-05-20 21:29:58 +0000

    adding ring buffer pieces using CPP

--- openmp/libomptarget/deviceRTLs/common/omptarget.h
+++ openmp/libomptarget/deviceRTLs/common/omptarget.h
@@ -338,6 +338,48 @@ INLINE uint32_t __kmpc_impl_popc(uint32_t x) { return __builtin_popcount(x); }
 INLINE uint32_t __kmpc_impl_ffs(uint64_t x) { return __builtin_ffsl(x); }
 INLINE uint32_t __kmpc_impl_popc(uint64_t x) { return __builtin_popcountl(x); }
 
+////////////////////////////////////////////////////////////////////////////////
+// device stack trace components
+////////////////////////////////////////////////////////////////////////////////
+template <class T>
+class ringBuffer {
+public:
+  explicit ringBuffer(size_t size) :
+    buf_(std::unique_ptr<T[]>(new T[size])),
+    max_size_(size)
+  { //empty constructor
+  }
+
+  void push(T item) {
+    buf_[head_] = item;
+    if(full_) {
+      tail_ = (tail_ + 1) % max_size_;
+    }
+
+    head_ = (head_ + 1) % max_size_;
+    full_ = head_ == tail_;
+  }
+
+  T pop() {
+    if(!full_ && (head_ == tail_)) {
+      return T();
+    }
+
+    auto val = buf_[tail_];
+    full_ = false;
+    tail_ = (tail_ + 1) % max_size_;
+
+    return val;
+  }
+
+private:
+  size_t head_ = 0;
+  size_t tail_ = 0;
+  const size_t max_size_;
+  bool full_ = 0;
+};
+
+
 #include "common/omptargeti.h"
 
 #endif
